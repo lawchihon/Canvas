69101473c9ef91e2e248f5ae9e59b1cf149f2026
John <lawchihon@ymail.com>
2017-04-10 00:25:24 -0700

    Creating Face with animation scale

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -15,12 +15,15 @@ class CanvasViewController: UIViewController {
     var trayDownOffset: CGFloat!
     var trayUp: CGPoint!
     var trayDown: CGPoint!
+    
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        trayDownOffset = 160
+        trayDownOffset = 200
         trayUp = trayView.center
         trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
     }
@@ -54,7 +57,7 @@ class CanvasViewController: UIViewController {
     
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: view)
-        var velocity = sender.velocity(in: view)
+        let velocity = sender.velocity(in: view)
         
         // When the gesture begins, store the tray's center into the trayOriginalCenter variable
         if sender.state == .began {
@@ -74,6 +77,55 @@ class CanvasViewController: UIViewController {
             else {
                 springAnimation(trayMove: self.trayUp)
             }
+        }
+    }
+
+    func scale(imageView: UIImageView, value: CGFloat) {
+        UIView.animate(withDuration:0.4,
+                       delay: 0.0,
+                       options: [],
+                       animations: {
+                           () -> Void in
+                           imageView.transform = CGAffineTransform(scaleX: value, y: value)
+                       },
+                       completion: nil)
+    }
+    
+    func scaleUp(imageView: UIImageView) {
+        scale(imageView: imageView, value: 2)
+    }
+    
+    func scaleDown(imageView: UIImageView) {
+        scale(imageView: imageView, value: 1)
+    }
+    
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+
+        // In the began state, create a new image view that contains the same image as the view that was panned on
+        if sender.state == .began {
+            // imageView now refers to the face that panned on.
+            let imageView = sender.view as! UIImageView
+            // Create a new image view that has the same image as the one currently panning.
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            // Add the new face to the main view.
+            view.addSubview(newlyCreatedFace)
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            // Since the original face is in the tray, but the new face is in the main view, offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            // Capture the initial center of the new face
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            // Scale of the face to be a little larger
+            scaleUp(imageView: newlyCreatedFace)
+        }
+        // In the changed state, pan the position of the newlyCreatedFace
+        else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        }
+        else if sender.state == .ended {
+            // Scale back to normal
+            scaleDown(imageView: newlyCreatedFace)
         }
     }
 }
